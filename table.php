4d2c869d5c215ec752624bbdb8fdc38b2adff973
sbaluyut23-0295@cca.edu.ph <sbaluyut23-0295@cca.edu.ph>
2025-02-19 18:42:25 +0800

    TABLE
    TABLE

---
+++ table.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Table Form</title>
+    </head>
+    <body>
+
+        <h1>student Record</h1>
+        <table id="Student_Record">
+        </table>
+
+    </body>
+</html>
